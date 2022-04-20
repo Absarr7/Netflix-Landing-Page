2dcc2c9aa162c7716e63b5933ab99de5fc4bfd7b
Absar Ahmad <absar4060@gmail.com>
2022-04-20 20:27:38 +0530

    faq section added

---
+++ JS/script.js
@@ -0,0 +1,86 @@
+// Question 1 
+
+const answer1 = document.getElementById('answer1');
+const btn1 = document.getElementById('question1');
+const icon1 = document.getElementById('plus1');
+
+btn1.onclick = function() {
+    if (answer1.style.display !== "none") {
+        answer1.style.display = "none";
+        icon1.className = "fa-solid fa-plus";
+    } else {
+        answer1.style.display = "block";
+        icon1.className = "fa-solid fa-xmark";
+
+    }
+}
+
+const answer2 = document.getElementById('answer2');
+const btn2 = document.getElementById('question2');
+const icon2 = document.getElementById('plus2');
+
+btn2.onclick = function() {
+    if (answer2.style.display !== "none") {
+        answer2.style.display = "none";
+        icon2.className = "fa-solid fa-plus";
+    } else {
+        answer2.style.display = "block";
+        icon2.className = "fa-solid fa-xmark";
+    }
+}
+
+const answer3 = document.getElementById('answer3');
+const btn3 = document.getElementById('question3');
+const icon3 = document.getElementById('plus3');
+
+btn3.onclick = function() {
+    if (answer3.style.display !== "none") {
+        answer3.style.display = "none";
+        icon3.className = "fa-solid fa-plus";
+    } else {
+        answer3.style.display = "block";
+        icon3.className = "fa-solid fa-xmark";
+    }
+}
+
+const answer4 = document.getElementById('answer4');
+const btn4 = document.getElementById('question4');
+const icon4 = document.getElementById('plus4');
+
+btn4.onclick = function() {
+    if (answer4.style.display !== "none") {
+        answer4.style.display = "none";
+        icon4.className = "fa-solid fa-plus";
+    } else {
+        answer4.style.display = "block";
+        icon4.className = "fa-solid fa-xmark";
+    }
+}
+
+const answer5 = document.getElementById('answer5');
+const btn5 = document.getElementById('question5');
+const icon5 = document.getElementById('plus5');
+
+btn5.onclick = function() {
+    if (answer5.style.display !== "none") {
+        answer5.style.display = "none";
+        icon5.className = "fa-solid fa-plus";
+    } else {
+        answer5.style.display = "block";
+        icon5.className = "fa-solid fa-xmark";
+    }
+}
+
+const answer6 = document.getElementById('answer6');
+const btn6 = document.getElementById('question6');
+const icon6 = document.getElementById('plus6');
+
+btn6.onclick = function() {
+    if (answer6.style.display !== "none") {
+        answer6.style.display = "none";
+        icon6.className = "fa-solid fa-plus";
+    } else {
+        answer6.style.display = "block";
+        icon6.className = "fa-solid fa-xmark";
+    }
+}
